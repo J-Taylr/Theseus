a669643db10c15bc37c9c9eda05a9d91e26cd7ee
J-Taylor <jmarcustaylor@gmail.com>
2020-10-14 15:41:05 +0100

    initial commit

---
+++ Assets/CharacterMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    public void PlayerMove()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) //w moves player forward one space 
+        {
+            var currentPos = gameObject.transform.position;
+            var newPosW = currentPos + Vector3.forward * 10;
+            gameObject.transform.position = newPosW;
+            currentPos = newPosW;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) // a moves player left one space 
+        {
+            var currentPos = gameObject.transform.position;
+            var newPosW = currentPos + Vector3.left * 10;
+            gameObject.transform.position = newPosW;
+            currentPos = newPosW;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)) // d moves player right one space 
+        {
+            var currentPos = gameObject.transform.position;
+            var newPosW = currentPos + Vector3.right * 10;
+            gameObject.transform.position = newPosW;
+            currentPos = newPosW;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)) // s moves player back one space 
+        {
+            var currentPos = gameObject.transform.position;
+            var newPosW = currentPos + Vector3.back * 10;
+            gameObject.transform.position = newPosW;
+            currentPos = newPosW;
+        }
+    }
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movement : MonoBehaviour
+{
+    public string steps;
+    public GameObject inputField;
+    public Vector3 position;
+
+    public void Move()
+    {
+        steps = inputField.GetComponent<Text>().text;
+
+
+    }
+}
