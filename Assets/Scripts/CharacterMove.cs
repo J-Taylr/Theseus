e4fe810452779edc44aba1ca1f2ad061c1acf6cc
J-Taylor <jmarcustaylor@gmail.com>
2020-10-22 13:18:53 +0100

    character now doesnt move into walls

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -17,12 +17,16 @@ public class CharacterMove : MonoBehaviour
     public string south = "south";
     public string east = "east";
     public string west = "west";
+    RaycastHit hit;
 
-
+    private void Start()
+    {
+        
+        
+    }
 
     public void PlayerMove()
     {
-
 
         steps = inputField.GetComponent<Text>().text;
         string[] words = steps.Split(' ');
@@ -32,43 +36,92 @@ public class CharacterMove : MonoBehaviour
 
 
 
+
+
         if (direction.Equals(north, StringComparison.OrdinalIgnoreCase)) //Moves to north
         {
+            float moveSpaces = stepsNum * 10;
+
+            RaycastHit hitNorth;
+            Physics.Raycast(cube.transform.position, Vector3.forward, out hitNorth);
 
             var currentPos = cube.transform.position;
-            var newPosW = currentPos + Vector3.forward * 10 * stepsNum;
+            var newPosW = currentPos + Vector3.forward * moveSpaces;
+            if (hitNorth.distance <= moveSpaces)
+            {
+                print("cannot Move here ");
+            }
+            else
+            { 
             cube.transform.position = newPosW;
             currentPos = newPosW;
-
-
+            }
         }
+
 
         if (direction.Equals(west, StringComparison.OrdinalIgnoreCase)) // a moves player left one space 
         {
+            float moveSpaces = stepsNum * 10;
+
+            RaycastHit hitWest;
+            Physics.Raycast(cube.transform.position, Vector3.left, out hitWest);
+
             var currentPos = cube.transform.position;
-            var newPosW = currentPos + Vector3.left * 10 * stepsNum;
-           cube.transform.position = newPosW;
+            var newPosW = currentPos + Vector3.left * moveSpaces;
+            if (hitWest.distance <= moveSpaces)
+            {
+                print("cannot Move here ");
+            }
+            else
+            {
+                
+            cube.transform.position = newPosW;
             currentPos = newPosW;
+            }
         }
 
         if (direction.Equals(east, StringComparison.OrdinalIgnoreCase))
         {
+            float moveSpaces = stepsNum * 10;
+
+            RaycastHit hitEast;
+            Physics.Raycast(cube.transform.position, Vector3.right, out hitEast);
+
             var currentPos = cube.transform.position;
-            var newPosW = currentPos + Vector3.right * 10 * stepsNum;
-            cube.transform.position = newPosW;
-            currentPos = newPosW;
+            var newPosW = currentPos + Vector3.right * moveSpaces;
+            if (hitEast.distance <= moveSpaces)
+            {
+                print("cannot Move here ");
+            }
+            else
+            {
+                cube.transform.position = newPosW;
+                currentPos = newPosW;                
+            }
         }
 
         if (direction.Equals(south, StringComparison.OrdinalIgnoreCase)) // s moves player back one space 
         {
+            float moveSpaces = stepsNum * 10;
+
+            RaycastHit hitSouth;
+            Physics.Raycast(cube.transform.position, Vector3.back, out hitSouth);
+
             var currentPos = cube.transform.position;
-            var newPosW = currentPos + Vector3.back * 10 * stepsNum;
-           cube.transform.position = newPosW;
-            currentPos = newPosW;
+            var newPosW = currentPos + Vector3.back * moveSpaces;
+            if (hitSouth.distance <= moveSpaces)
+            {
+                print("cannot Move here ");
+            }
+            else
+            {
+             cube.transform.position = newPosW;
+             currentPos = newPosW;                
+            }
             print("im here");
         }
     }
 
-
+   
 
 }
